016d3ffd6254d51d5c42cc0bf7ee46f4de5bcb07
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2025-12-30 19:42:56 +0100

    adding bootstrapping for client secret and other small fixes

--- src/main/webui/src/app/autofocus.directive.spec.ts
+++ src/main/webui/src/app/autofocus.directive.spec.ts
@@ -1,8 +1,21 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { ElementRef } from '@angular/core';
 
 describe('AutofocusDirective', () => {
   it('should create an instance', () => {
-    const directive = new AutofocusDirective();
+    const mockElementRef = new ElementRef(document.createElement('input'));
+    const directive = new AutofocusDirective(mockElementRef);
     expect(directive).toBeTruthy();
+  });
+
+  it('should focus element after view init', () => {
+    const inputElement = document.createElement('input');
+    const mockElementRef = new ElementRef(inputElement);
+    const directive = new AutofocusDirective(mockElementRef);
+    
+    spyOn(inputElement, 'focus');
+    directive.ngAfterViewInit();
+    
+    expect(inputElement.focus).toHaveBeenCalled();
   });
 });
